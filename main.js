9241ecc99f70ad10ad68ca567d1610e887ba3091
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2022-11-30 16:19:39 +0200

    task

---
+++ main.js
@@ -0,0 +1,10 @@
+Parašykite programą, kuri turėtų tris kintamuosius, saugančius galimo trikampio kraštinių
+ilgių reikšmes.
+Panaudodami duotus kintamuosius nustatykite koks trikampis gali susidaryti su duotomis
+kraštinėmis - lygiakraštis, lygiašonis, status, įvairiakraštis ar joks. 
+Pasirinktu būdu atspausdinkite koks trikampis susidaro ir jei susidaro.
+Patikrinkite savo programą su tokiais duomenimis:
+1) 3, 4, 5
+2) 8, 2, 10
+3) 4, 4, 4
+4) 6, 7, 6
